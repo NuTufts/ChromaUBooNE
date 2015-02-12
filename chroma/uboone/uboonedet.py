456720055deb258ae0d9a01e1f630943c37a14aa
Renatechorate <renatechordate@gmail.com>
2015-02-12 14:30:03 -0500

    added wire plane modification hook. kind of hacky.

--- chroma/uboone/uboonedet.py
+++ chroma/uboone/uboonedet.py
@@ -7,12 +7,14 @@ import chroma.uboone.surfaces as uboonesurfaces
 from chroma.loader import load_bvh
 from chroma.bvh.NodeDSAR import NodeDSARtree
 import time
+from sets import Set
 
 # Geometry class representing the MicroBooNE geometry
 
 class ubooneDet( Detector ):
     def __init__(self, daefile, acrylic_detect=True, acrylic_wls=False,
                  bvh_name="uboone_bvh_default", detector_volumes=[],
+                 wireplane_volumes=[],
                  auto_build_bvh=True, read_bvh_cache=True, calculate_ndsar_tree=False,
                  update_bvh_cache=True, cache_dir=None, bvh_method='grid', bvh_target_degree='3',
                  cuda_device=None, cl_device=None, dump_node_info=False ):
@@ -34,8 +36,30 @@ class ubooneDet( Detector ):
 
         # We use g4dae tools to create geometry with mesh whose triangles have materials assigned to them
         DAENode.parse( daefile, sens_mats=[] )
-        self.collada2chroma = ColladaToChroma( DAENode, dump_node_info=dump_node_info )
-        geom = self.collada2chroma.convert_geometry()
+        self.collada2chroma = ColladaToChroma( DAENode, dump_node_info=False )
+        geom = self.collada2chroma.convert_geometry_partial()
+
+        # Look for wireplane alogorithms
+        if len(wireplane_volumes)>0:
+            wireplaneset = map( lambda x: x[0], wireplane_volumes )
+            for n,solid in enumerate(geom.solids):
+                node = solid.node
+                if node.pv.id in wireplaneset:
+                    for wireplane in wireplane_volumes:
+                        if wireplane[0]==node.pv.id:
+                            wireplane[1]( solid )
+            print "Found Solids to add wireplanes: ",len(wireplaneset)
+                    
+        geom = self.collada2chroma.finish_converting_geometry()
+
+        if dump_node_info:
+            for n,solid in enumerate(geom.solids):
+                node = solid.node
+                mesh = solid.mesh
+                material2names = map( lambda x: x.name, np.unique(solid.material2) )
+                material1names = map( lambda x: x.name, np.unique(solid.material1) )
+                surfaces = map( lambda x: x.name, filter( lambda x: x!=None, solid.unique_surfaces ) )
+                print "[SOLID %d, NODE %05d:%s,%s]"%(n, node.index,node.pv.id,node.lv.id)," NTriangles=%d OuterMat=%s InnerMat=%s Surface=%s"%(len(mesh.triangles), material2names, material1names,surfaces)
         
         # copy member objects (maybe use copy module instead?)
         self.mesh = geom.mesh
@@ -48,11 +72,18 @@ class ubooneDet( Detector ):
         material_lookup = dict(zip(self.unique_materials, range(len(self.unique_materials))))
 
         # Next we need to go through all the triangles and make sure they have the right surfaces attached to them
+        # We use the material lists to loop over all triangles
         self.unique_surfaces = []
         surface_index_dict = {}
         surface_indices = []
         for id, mats in enumerate( zip(self.material1_index, self.material2_index) ):
-            surface = uboonesurfaces.get_boundary_surface( self.unique_materials[mats[0]].name, self.unique_materials[mats[1]].name )
+            existing_surface = geom.surface_index[ id ]
+            if existing_surface!=-1:
+                print "Triangle %d already has specified surface: ",geom.unique_surfaces[ existing_surface ]
+                #surface_indices.append( existing_surface )
+                surface = geom.unique_surfaces[ existing_surface ]
+            else:
+                surface = uboonesurfaces.get_boundary_surface( self.unique_materials[mats[0]].name, self.unique_materials[mats[1]].name )
             if surface==None:
                 surface_indices.append( -1 )
             else:
@@ -62,6 +93,7 @@ class ubooneDet( Detector ):
                 surface_indices.append( surface_index_dict[ surface ] )
         self.surface_index = np.array( surface_indices, dtype=np.int )
         print "number of surface indicies: ",len(self.surface_index)
+
 
         # Finally, setup channels
         print "SETUP UBOONE CHANNELS from solids list: ",len(self.solids)
