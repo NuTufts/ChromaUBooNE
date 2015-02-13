9a6101806cdaf0c8b75099eef37c8e1491e12b03
Taritreee Wongjirad <twongjirad@Taritreees-MacBook-Pro.local>
2015-02-13 17:13:05 -0500

    added class for importgeo module.  also tried to improve documentaton.

--- README.md
+++ README.md
@@ -1,35 +1,44 @@
 # Chroma Uboone
 
 Fork of Chroma developed for the use with the MicroBooNE neutrino experiment.  
-Actually, this is a fork of Simon C. Blythe's fork of Chroma.
+Actually, this is a fork of Simon C. Blyth's fork of Chroma.
 
 ## Modifications by this fork:
 
-* Modified gpu/tools.py to be able to load context with cuda context flags.  By default, loads the flag for device-mapped host memory.
+* Modified gpu/tools.py to be able to load context with cuda context flags.  
+  By default, loads the flag for device-mapped host memory. 
+  Needed for running on an older card (C1080).
 * Fixed what seemed like a bug in detector.py where the pdf was defined with one too many bins.
 * Fixed a bug in gpu/tools.py: make_gpu_struct.  There were problems with the numpy buffer interface.
 * Modifications have been made to test functions that were used to help explore program behavior.  Some functions have been added.
-* Working on allowing running on OpenCL
+* Added abilit to run with OpenCL, using pyOpenCL
+* Providing new module, importgeo, that uses tools written by S.C. Blyth to import geometries described in
+  COLLADA file output by G4DAE C++ library for Geant4
+* Having ROOT with PyROOT extension will allow some tests to make plots. 
+  Not required (or at least tried not to make it required).
 
 ## OpenCL capability
 
 * API, CUDA or OpenCL, is selectable using functions in api.py
 * api.py also has functions to test which API
 * opencl has some big differences that affect chroma: no device-mapped memory, no built-in random number generator, and different struct creation/definition.
+* because of these differences, port was not done so cleanly.  many modules have been reorganized and classes changed. merging this fork may require coordination.
 
-## BVH Precision differences between opencl and cuda:
+### BVH Precision differences between opencl and cuda:
 
-* I see some precision effects that causes differences in the BVH tree between opencl and cuda.  Tests used the companion cube mesh.
+* I see some precision effects which causes differences in the BVH tree between opencl and cuda.  Tests used the companion cube mesh.
 * Get differences in quantize centroid values which then propagates to rest of tree structure.
 * From my studies, differences are only off by one quantum along any axis
-* Example: Mismatch: ID=286  cuda= 114847925804877  cl= 114847925804876
-  CUDA:  [37663, 55944, 4589]  CL:  [37662, 55944, 4589]  diff= [1 0 0]
+* Example Mismatch: Triangle ID=286, morton codes are  cuda= 114847925804877  cl= 114847925804876
+* Bounding box for mismatch is:
+  CUDA=[37663, 55944, 4589],  CL=[37662, 55944, 4589], diff= [1 0 0]
 * CUDA value always seems to be bigger
-* For companion cube test about 1.9% of all morton codes are different
+* For companion cube mesh about 1.9% of all morton codes are different
 
-## Random Number Generator
+### Random Number Generator
 
 * Unlike CUDA, OpenCL does not have a built-in random number generator
 * Implemented RNG using Random123 package: http://www.deshawresearch.com/downloads/download_random123.cgi/. Included in repo.
-* Random123 is in principle stateless. Created state vector in gpu/clrandstate.py. Implemented uniform sampler in cl/random.cl
+* Random123 is in principle stateless. Created state vector in gpu/clrandstate.py. Implemented uniform sampler in cl/random.cl. 
+Require state, because being able to reproduce results is a requirement for particle physics MC.
 * tests, cltest/test_randomgen.py and cltest/test_sample_cdf.py, generate flat and gaussian distribution, respectively. Seems to work.
---
+++ chroma/importgeo/g4daedetector.py
@@ -0,0 +1,192 @@
+import os,sys
+import numpy as np
+from chroma.detector import Detector
+from chroma.g4daenode.collada_to_chroma import ColladaToChroma
+from chroma.g4daenode.g4daenode import DAENode
+import chroma.uboone.surfaces as uboonesurfaces
+from chroma.loader import load_bvh
+from chroma.bvh.NodeDSAR import NodeDSARtree
+import time
+from sets import Set
+
+# Geometry class representing the MicroBooNE geometry
+
+class G4DAEDetector( Detector ):
+    def __init__(self, daefile, acrylic_detect=True, acrylic_wls=False,
+                 bvh_name="bvh_default", detector_volumes=[],
+                 wireplane_volumes=[],
+                 auto_build_bvh=True, read_bvh_cache=True, calculate_ndsar_tree=False,
+                 update_bvh_cache=True, cache_dir=None, bvh_method='grid', bvh_target_degree='3',
+                 cuda_device=None, cl_device=None, dump_node_info=False ):
+        """
+        Loads a geometry for Chroma built using the COLLADA file exported by G4DAE.
+        
+        Parameters
+        ----------
+        daefile : str
+          string with path to COLLADA file
+        acrylic_detect : bool
+           hacky way to specify detector behavior. needs to be removed
+        acrylic_wls : bool
+           hacky way to specify that acrylic material should wavelength shift. needs to be removed
+        bvh_name : string
+           specifies name of BVH cache written to working directory
+        detector_volumes : list of str
+           logical volume names that will be marked as a sensitive detectors
+        wireplane_volumes : list of (str,function) tuples
+           If a Solid with an associated physical volume whose name that matches 'str', 
+           we give the function the Solid instance.  The intention is for the user to provide 
+           one or more of the triangles in the Solid with a wireplane surface.
+           For an example see: XXX
+        auto_build_bvh : bool
+        read_bvh_cache : bool
+        update_bvh_cache : bool
+        cache_dir : str
+        bvh_method : str
+        bvh_target_degree : str of number
+        cuda_device : pycuda device instance
+           build and store geometry on specified CUDA device
+        cl_device : pyopencl device instance
+           build and store geometry on specified OpenCL device
+        calculate_ndsar_tree : bool
+           after geometry specified, traverse BVH and build ropes.  deprecated and will likely be removed
+        dump_node_info : bool
+           if true, will list all of the nodes in the geometry
+
+        Returns
+        -------
+        instance of object
+
+        Raises
+        ------
+        ValueError
+           if both acrylic_detect and acrylic_wls are True. Need to remove.
+        ValueError
+           when no detector volumes are specified. likely will be removed.
+        """
+
+        if acrylic_detect and acrylic_wls:
+            raise ValueError('cannot have acrylic act as both detector and wavelength shifter')
+        # run constructor of base class
+        if acrylic_detect:
+            super( ubooneDet, self ).__init__( "Acrylic" )
+        elif acrylic_wls:
+            super( ubooneDet, self ).__init__( "bialkali" )
+        else:
+            print "Warning: no detector material specified"
+        self.acrylic_detect = acrylic_detect
+        self.acrylic_wls    = acrylic_wls
+        
+        if len( detector_volumes )==0:
+            raise ValueError( "No detector volumes specified!" )
+
+        # We use g4dae tools to create geometry with mesh whose triangles have materials assigned to them
+        DAENode.parse( daefile, sens_mats=[] )
+        self.collada2chroma = ColladaToChroma( DAENode, dump_node_info=False )
+        geom = self.collada2chroma.convert_geometry_partial()
+
+        # Look for wireplane alogorithms
+        if len(wireplane_volumes)>0:
+            wireplaneset = map( lambda x: x[0], wireplane_volumes )
+            for n,solid in enumerate(geom.solids):
+                node = solid.node
+                if node.pv.id in wireplaneset:
+                    for wireplane in wireplane_volumes:
+                        if wireplane[0]==node.pv.id:
+                            wireplane[1]( solid )
+            print "Found Solids to add wireplanes: ",len(wireplaneset)
+                    
+        geom = self.collada2chroma.finish_converting_geometry()
+
+        if dump_node_info:
+            for n,solid in enumerate(geom.solids):
+                node = solid.node
+                mesh = solid.mesh
+                material2names = map( lambda x: x.name, np.unique(solid.material2) )
+                material1names = map( lambda x: x.name, np.unique(solid.material1) )
+                surfaces = map( lambda x: x.name, filter( lambda x: x!=None, solid.unique_surfaces ) )
+                print "[SOLID %d, NODE %05d:%s,%s]"%(n, node.index,node.pv.id,node.lv.id)," NTriangles=%d OuterMat=%s InnerMat=%s Surface=%s"%(len(mesh.triangles), material2names, material1names,surfaces)
+        
+        # copy member objects (maybe use copy module instead?)
+        self.mesh = geom.mesh
+        self.colors = geom.colors
+        self.solids = geom.solids
+        self.solid_id = geom.solid_id
+        self.unique_materials = geom.unique_materials
+        self.material1_index = geom.material1_index
+        self.material2_index = geom.material2_index
+        material_lookup = dict(zip(self.unique_materials, range(len(self.unique_materials))))
+
+        # Next we need to go through all the triangles and make sure they have the right surfaces attached to them
+        # We use the material lists to loop over all triangles
+        self.unique_surfaces = []
+        surface_index_dict = {}
+        surface_indices = []
+        for id, mats in enumerate( zip(self.material1_index, self.material2_index) ):
+            existing_surface = geom.surface_index[ id ]
+            if existing_surface!=-1:
+                print "Triangle %d already has specified surface: "%(id),geom.unique_surfaces[ existing_surface ]
+                surface = geom.unique_surfaces[ existing_surface ]
+            else:
+                surface = uboonesurfaces.get_boundary_surface( self.unique_materials[mats[0]].name, self.unique_materials[mats[1]].name )
+            if surface==None:
+                surface_indices.append( -1 )
+            else:
+                if surface not in self.unique_surfaces:
+                    self.unique_surfaces.append( surface )
+                    surface_index_dict[ surface ] = self.unique_surfaces.index( surface )
+                surface_indices.append( surface_index_dict[ surface ] )
+        self.surface_index = np.array( surface_indices, dtype=np.int )
+        print "number of surface indicies: ",len(self.surface_index)
+
+
+        # Finally, setup channels
+        print "SETUP UBOONE CHANNELS from solids list (",len(self.solids)," solids)"
+        self.solid_id_to_channel_index.resize( len(self.solids) )
+        self.solid_id_to_channel_index.fill(-1) # default no channels
+        self.solid_id_to_channel_id.resize( len(self.solids) )
+        self.solid_id_to_channel_id.fill(-1)
+
+        print len( self.solid_id_to_channel_index ), len(  self.solid_id_to_channel_id )
+        # prevous calls to add_solid by collada_to_chroma sized this array
+        for n,solid in enumerate(self.solids):
+            if acrylic_detect and  any( volnames in solid.node.lv.id for volnames in detector_volumes ):
+                solid_id = n
+                channel_index = len(self.channel_index_to_solid_id)
+                channel_id = channel_index # later can do more fancy channel indexing/labeling
+                self.solid_id_to_channel_index[solid_id] = channel_index
+                self.solid_id_to_channel_id[solid_id] = channel_id
+
+                # resize channel_index arrays before filling
+                self.channel_index_to_solid_id.resize(channel_index+1)
+                self.channel_index_to_solid_id[channel_index] = solid_id
+                self.channel_index_to_channel_id.resize(channel_index+1)
+                self.channel_index_to_channel_id[channel_index] = channel_id
+                
+                # dictionary does not need resizing
+                self.channel_id_to_channel_index[channel_id] = channel_index
+        print "Number of Channels Added: ",len(self.channel_index_to_solid_id)
+
+        if self.bvh is None:
+            self.bvh = load_bvh(self, auto_build_bvh=auto_build_bvh,
+                                read_bvh_cache=read_bvh_cache,
+                                update_bvh_cache=update_bvh_cache,
+                                cache_dir=cache_dir, bvh_method=bvh_method, target_degree=bvh_target_degree,
+                                cuda_device=cuda_device, cl_device=cl_device)
+
+        self._setup_photodetectors()
+
+        if calculate_ndsar_tree:
+            # This tree helps with the navigation of the node tree (deprecated)
+            print "Calculate node DSAR tree ...",
+            sndsar = time.time()
+            self.node_dsar_tree = NodeDSARtree( self.bvh )
+            endsar = time.time()
+            print " done ",endsar-sndsar," secs."
+
+
+        # OK, we should be ready to go
+    def _setup_photodetectors( self ):
+        self.set_time_dist_gaussian( 1.2, -6.0, 6.0 )
+        self.set_charge_dist_gaussian( 1.0, 0.1, 0.5, 1.5 )
+        
